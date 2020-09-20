3548418a64a14973ced1a6643387e870eb26f1a2
Samuel Girardin <samuelgirardin@pm.me>
2020-09-21 01:26:34 +0200

    add prettier term output

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import os
 
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m [WARNING]: "
+INFO = "\x1b[1;33m [INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
@@ -12,5 +18,6 @@ if 'Not open source' == '{{ cookiecutter.open_source_license }}':
     remove_file('LICENSE')
 
 # Add Service Provider if masonite globally available
-print("""You should now run:\n* python craft provider {{ cookiecutter.project_name|replace(' ', '') }}
-\n* move it to src/masonite/{{cookiecutter.pkg_name}}/providers\n""")
+# print("""You should now run:\n* python craft provider {{ cookiecutter.project_name|replace(' ', '') }}
+# \n* move it to src/masonite/{{cookiecutter.pkg_name}}/providers\n""")
+print(SUCCESS+"Your package is ready to be developed ! => cd {{cookiecutter.project_slug}}/"+TERMINATOR)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,11 +1,17 @@
 import sys
 
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m [WARNING]: "
+INFO = "\x1b[1;33m [INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
 
 pkg_name = '{{ cookiecutter.pkg_name }}'
 
 
 if "masonite" in pkg_name:
-    print('ERROR: %s is not a valid Masonite package! Reasons: package name cannot contains masonite.' % pkg_name)
+    print(WARNING + '%s is not a valid Masonite package! Reasons: package name cannot contains masonite.' % pkg_name + TERMINATOR)
 
     # exits with status 1 to indicate failure
     sys.exit(1)
