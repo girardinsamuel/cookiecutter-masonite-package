9362768867b2f6b697c3910de20991fc604cfb59
Samuel Girardin <samuelgirardin@pm.me>
2021-05-03 14:54:41 +0200

    fix description length

--- generate_demo_project.py
+++ generate_demo_project.py
@@ -5,13 +5,6 @@ import shutil
 import sys
 from cookiecutter.main import cookiecutter
 
-# display info on the README about this cookiecutter thing
-project_description = """Demo package to show Masonite cookiecutter scaffolding result.
-
-**This package has been generated with [Cookiecutter Masonite Package](https://github.com/girardinsamuel/cookiecutter-masonite-package). The badges will display correctly at first Github release and first PyPi publish.**
-
-**To report bugs or request features please use the [Cookiecutter Masonite Package](https://github.com/girardinsamuel/cookiecutter-masonite-package) repo directly.**"""
-
 # Clean old generated masonite demo package which are not cleaned by cookiecutter because hidden
 shutil.rmtree("../masonite-demo-package/.github/", ignore_errors=True)
 
@@ -33,7 +26,7 @@ cookiecutter(
     checkout=tag,
     extra_context={
         "project_name": "Demo Package",
-        "project_description": project_description,
+        "project_description": "Demo package to show Masonite package generator.",
     },
 )
 
