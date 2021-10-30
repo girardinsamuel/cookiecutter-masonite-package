1ad1d2e953153074c4b2e2250b5176af2f5c69a9
Samuel Girardin <samuelgirardin@pm.me>
2021-10-30 15:34:04 +0200

    fix wsgi build

--- {{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.project_slug}}/wsgi.py
@@ -1,11 +1,10 @@
 from masonite.foundation import Application, Kernel
 from tests.integrations.config.providers import PROVIDERS
-from tests.integrations.app.Kernel import Kernel as ApplicationKernel
-import os
+from tests.integrations.Kernel import Kernel as ApplicationKernel
 
 
 """Start The Application Instance."""
-application = Application(os.getcwd())
+application = Application("tests/integrations")
 
 """Now Bind important providers needed to make the framework work."""
 application.register_providers(Kernel, ApplicationKernel)
